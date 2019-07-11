6dc86826b1f77c71f5ab357b9346c482b08a01a3
Lachlan Phillips <lachlan.phillips.00@gmail.com>
2019-07-11 10:13:09 +1000

    Correct spelling mistakes in analysis zone names

--- program_analyseCounts.py
+++ program_analyseCounts.py
@@ -5,7 +5,7 @@ import pandas as pd
 from data_visulisation import *
 
 # read in data
-count_Jarv = pd.read_csv('./data/count_Jarv.csv', parse_dates=['dt'],
+count_Jerv = pd.read_csv('./data/count_Jerv.csv', parse_dates=['dt'],
                         infer_datetime_format=True)
 count_Bate = pd.read_csv('./data/count_Bate.csv', parse_dates=['dt'],
                         infer_datetime_format=True)
@@ -13,10 +13,10 @@ count_Gabo = pd.read_csv('./data/count_Gabo.csv', parse_dates=['dt'],
                         infer_datetime_format=True)
 
 # make plots
-seasonal_change_analysis(count_Jarv, 'Jarvis Bay', './plots/seasonal_Jarv.png')
-seasonal_change_analysis(count_Bate, 'Batesmen Bay', './plots/seasonal_Bate.png')
+seasonal_change_analysis(count_Jerv, 'Jervis Bay', './plots/seasonal_Jerv.png')
+seasonal_change_analysis(count_Bate, 'Batemans Bay', './plots/seasonal_Bate.png')
 seasonal_change_analysis(count_Gabo, 'Gabo Island', './plots/seasonal_Gabo.png')  
 
 # join 3 plots together (temp hack)
-img_ls = ['./plots/seasonal_Jarv.png', './plots/seasonal_Bate.png', './plots/seasonal_Gabo.png']
+img_ls = ['./plots/seasonal_Jerv.png', './plots/seasonal_Bate.png', './plots/seasonal_Gabo.png']
 img_join('./plots/seasonal_All.png', img_ls, direction='vertical')
--- program_obtainData.py
+++ program_obtainData.py
@@ -15,12 +15,12 @@ ROMS_directory = "/Users/lachlanphillips/PhD_Large_Data/ROMS/Montague_subset"
 sourceboxA = [155, 160, -28.5, -22.5]
 sourceboxB = [155, 160, -46, -41]
 # Study zones (center point and radius)
-JarvBox = boxmaker(150.9451487, -35.113566, 50)
+JervBox = boxmaker(150.9451487, -35.113566, 50)
 BateBox = boxmaker(150.2269, -36.25201, 50)
 GaboBox = boxmaker(150.2925979, -37.586966, 50)
 
 # check study zone box positions
-check_boxROMS([JarvBox, BateBox, GaboBox], ROMS_directory, 
+check_boxROMS([JervBox, BateBox, GaboBox], ROMS_directory, 
 			depthmax=4000, save=True, out_fn='./plots/study_zones.png')
 
 # produce training data
@@ -30,11 +30,11 @@ train_CARS(CARS_directory, './data/', sourceboxA, sourceboxB, plot_boxes=True)
 lr_model = current_model('./data/training_data.csv')
 
 # get count data for regions
-count_Jarv = analyse_region_counts(ROMS_directory, lr_model, JarvBox, depthmax=4000)
+count_Jarv = analyse_region_counts(ROMS_directory, lr_model, JervBox, depthmax=4000)
 count_Bate = analyse_region_counts(ROMS_directory, lr_model, BateBox, depthmax=4000)
 count_Gabo = analyse_region_counts(ROMS_directory, lr_model, GaboBox, depthmax=4000)
 
 # save data
-count_Jarv.to_csv('./data/count_Jarv.csv', index=False)
+count_Jarv.to_csv('./data/count_Jerv.csv', index=False)
 count_Bate.to_csv('./data/count_Bate.csv', index=False)
 count_Gabo.to_csv('./data/count_Gabo.csv', index=False)
