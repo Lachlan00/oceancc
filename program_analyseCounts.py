12be08749ea461a91c9f23388eadd56d856c1610
Lachlan Phillips <lachlan.phillips.00@gmail.com>
2019-07-31 11:21:16 +1000

    Change 'Gabo' to 'Howe'

--- program_analyseCounts.py
+++ program_analyseCounts.py
@@ -9,14 +9,18 @@ count_Jerv = pd.read_csv('./data/count_Jerv.csv', parse_dates=['dt'],
                         infer_datetime_format=True)
 count_Bate = pd.read_csv('./data/count_Bate.csv', parse_dates=['dt'],
                         infer_datetime_format=True)
-count_Gabo = pd.read_csv('./data/count_Gabo.csv', parse_dates=['dt'],
+count_Howe = pd.read_csv('./data/count_Howe.csv', parse_dates=['dt'],
                         infer_datetime_format=True)
 
-# make plots
+# make seasonality plots
 seasonal_change_analysis(count_Jerv, 'Jervis Bay', './plots/seasonal_Jerv.png')
 seasonal_change_analysis(count_Bate, 'Batemans Bay', './plots/seasonal_Bate.png')
-seasonal_change_analysis(count_Gabo, 'Gabo Island', './plots/seasonal_Gabo.png')  
-
+seasonal_change_analysis(count_Howe, 'Cape Howe', './plots/seasonal_Howe.png')  
 # join 3 plots together (temp hack)
-img_ls = ['./plots/seasonal_Jerv.png', './plots/seasonal_Bate.png', './plots/seasonal_Gabo.png']
+img_ls = ['./plots/seasonal_Jerv.png', './plots/seasonal_Bate.png', './plots/seasonal_Howe.png']
 img_join('./plots/seasonal_All.png', img_ls, direction='vertical')
+
+# make monthly chnage plots
+seasonal_change_analysis(count_Jerv, 'Jervis Bay', './plots/monthly_Jerv.png')
+seasonal_change_analysis(count_Bate, 'Batemans Bay', './plots/monthly_Bate.png')
+seasonal_change_analysis(count_Howe, 'Cape Howe', './plots/monthly_Howe.png')
--- program_obtainData.py
+++ program_obtainData.py
@@ -9,18 +9,18 @@ from data_visulisation import *
 # Configuration #
 #################
 # directories
-CARS_directory = '/Users/lachlanphillips/PhD_Large_Data/CARS/'
-ROMS_directory = "/Users/lachlanphillips/PhD_Large_Data/ROMS/Montague_subset"
+CARS_directory = '/Users/lachlanphillips/PhD_Large_Data/CARS'
+ROMS_directory = '/Users/lachlanphillips/PhD_Large_Data/ROMS/Montague_subset'
 # Training sources
 sourceboxA = [155, 160, -28.5, -22.5]
 sourceboxB = [155, 160, -46, -41]
 # Study zones (center point and radius)
 JervBox = boxmaker(150.9451487, -35.113566, 50)
 BateBox = boxmaker(150.2269, -36.25201, 50)
-GaboBox = boxmaker(150.2925979, -37.586966, 50)
+HoweBox = boxmaker(150.2925979, -37.586966, 50)
 
 # check study zone box positions
-check_boxROMS([JervBox, BateBox, GaboBox], ROMS_directory, 
+check_boxROMS([JervBox, BateBox, HoweBox], ROMS_directory, 
 			depthmax=4000, save=True, out_fn='./plots/study_zones.png')
 
 # produce training data
@@ -32,9 +32,9 @@ lr_model = current_model('./data/training_data.csv')
 # get count data for regions
 count_Jarv = analyse_region_counts(ROMS_directory, lr_model, JervBox, depthmax=4000)
 count_Bate = analyse_region_counts(ROMS_directory, lr_model, BateBox, depthmax=4000)
-count_Gabo = analyse_region_counts(ROMS_directory, lr_model, GaboBox, depthmax=4000)
+count_Howe = analyse_region_counts(ROMS_directory, lr_model, HoweBox, depthmax=4000)
 
 # save data
 count_Jarv.to_csv('./data/count_Jerv.csv', index=False)
 count_Bate.to_csv('./data/count_Bate.csv', index=False)
-count_Gabo.to_csv('./data/count_Gabo.csv', index=False)
+count_Howe.to_csv('./data/count_Howe.csv', index=False)
